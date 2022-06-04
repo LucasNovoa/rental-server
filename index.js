c51a4ae96d4f9035ac44aab6c69d88ba32cfe916
Pato9310 <patito.mdd@gmail.com>
2022-06-03 21:31:25 -0300

    deploy heroku

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://rental-app-client.netlify.app', 'https://rental-app-server.herokuapp.com', 'https://rental-dashboard-opal.vercel.app'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://rental-x.netlify.app', 'https://git.heroku.com/rental-x-server.git', 'https://rental-dashboard-opal.vercel.app'];
 
 const corsOptions = {
   origin(origin, callback) {
